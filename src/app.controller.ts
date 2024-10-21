6be35bb45e8e7d15b8ad7cdb60b9286ed64ad9ca
Moha0170 <mohalaja06@gmail.com>
2024-10-21 14:04:04 +0200

    nem

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Param, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -11,5 +11,34 @@ export class AppController {
     return {
       message: this.appService.getHello()
     };
+  }
+
+  #products = [
+    {
+      nev: "valami1",
+      ar: 1000
+    },
+    {
+      nev: "valami2",
+      ar: 2000
+    },
+    {
+      nev: "valami3",
+      ar: 3000
+    },
+    {
+      nev: "valami4",
+      ar: 4000
+    }
+  ];
+
+  @Get("products")
+  listProducts() {
+    return this.#products
+  }
+
+  @Get("products/:id")
+  getProduct(@Param("id") id: string) {
+    return this.#products[Number(id)-1];
   }
 }
